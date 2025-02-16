69c7c1c5b69d041e549ec10b7452d2390bea109a
N12090809 <nosawa1209@gmail.com>
2025-02-15 21:44:13 -0600

    Update serve_static.py

--- serve_static.py
+++ serve_static.py
@@ -1,10 +1,14 @@
 import http.server
 import socketserver
+import os
 
 PORT = 8000
 
-Handler = http.server.SimpleHTTPRequestHandler
+class StaticHandler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory="static", **kwargs)
 
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
+with socketserver.TCPServer(("", PORT), StaticHandler) as httpd:
     print(f"Serving static files on port {PORT}")
     httpd.serve_forever()
+
